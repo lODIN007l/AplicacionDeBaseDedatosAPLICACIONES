d358ca7fd9a0b1618a2484b12a8e11192f7e16a9
lODIN007l <odineldios3@gmail.com>
2021-07-19 19:25:31 -0500

    first commit

--- APLICACIONBASEDEDATOS/APLICACIONBASEDEDATOS/Program.cs
+++ APLICACIONBASEDEDATOS/APLICACIONBASEDEDATOS/Program.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Capa_Interfaz
+namespace APLICACIONBASEDEDATOS
 {
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.Run(new Form1());
         }
     }
 }
