d4aade084232645b242d553267ff3fc0f0332b4b
MarvinSoft <marvin.erazo@espoch.edu.ec>
2021-07-20 18:00:33 -0500

    Creacion de la funciones en capalogica y capabase

--- APLICACIONBASEDEDATOS/Capa_BD/OperacionesBD.cs
+++ APLICACIONBASEDEDATOS/Capa_BD/OperacionesBD.cs
@@ -3,10 +3,225 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Capa_BD
 {
-    class OperacionesBD
+    public class OperacionesBD
     {
+        Conexion objConectar = new Conexion();
+
+        public DataTable MostrarUsuarios()
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                objConectar.Abrir();
+                SqlCommand comando = new SqlCommand("comando", objConectar.conectar);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception exc)
+            {
+                objConectar.Cerrar();
+                tabla = null;
+            }
+            return tabla;
+        }
+
+        public DataTable MostrarCursos()
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                objConectar.Abrir();
+                SqlCommand comando = new SqlCommand("comando", objConectar.conectar);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception exc)
+            {
+                objConectar.Cerrar();
+                tabla = null;
+            }
+            return tabla;
+        }
+
+        public DataTable MostrarVideos(int cod_curso)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                objConectar.Abrir();
+                SqlCommand comando = new SqlCommand("comando", objConectar.conectar);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception exc)
+            {
+                objConectar.Cerrar();
+                tabla = null;
+            }
+            return tabla;
+        }
+
+        public DataTable MostrarComprados(int cod_curso)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                objConectar.Abrir();
+                SqlCommand comando = new SqlCommand("comando", objConectar.conectar);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception exc)
+            {
+                objConectar.Cerrar();
+                tabla = null;
+            }
+            return tabla;
+        }
+
+        public DataTable BuscarUsuario(string nomb)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                objConectar.Abrir();
+                SqlCommand comando = new SqlCommand("comando", objConectar.conectar);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch(Exception exc)
+            {
+                objConectar.Cerrar();
+                tabla = null;
+            }
+            return tabla;
+
+        }
+
+        public DataTable BuscarCurso(int cod)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                objConectar.Abrir();
+                SqlCommand comando = new SqlCommand("comando", objConectar.conectar);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception exc)
+            {
+                objConectar.Cerrar();
+                tabla = null;
+            }
+            return tabla;
+        }
+
+        public void IngresarUsuario(string nombre, string correo, string password)
+        {
+            objConectar.Abrir();
+            string cmInsertar = "comando";
+            SqlCommand comando = new SqlCommand(cmInsertar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
+        public void IngresarCurso(string titulo, string lenguaje, string descripcion, string precio)
+        {
+            objConectar.Abrir();
+            string cmInsertar = "comando";
+            SqlCommand comando = new SqlCommand(cmInsertar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
+        public void IngresarVideo(string titulo, string duracion)
+        {
+            objConectar.Abrir();
+            string cmInsertar = "comando";
+            SqlCommand comando = new SqlCommand(cmInsertar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+        public void CursoComprado(int cod_usu, int cod_video)
+        {
+            objConectar.Abrir();
+            string cmInsertar = "comando";
+            SqlCommand comando = new SqlCommand(cmInsertar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+        
+        public void EliminarUsuario(int cod)
+        {
+            objConectar.Abrir();
+            string cmInsertar = "comando";
+            SqlCommand comando = new SqlCommand(cmInsertar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
+        public void EliminarCurso(int cod)
+        {
+            objConectar.Abrir();
+            string cmEliminar = "comando";
+            SqlCommand comando = new SqlCommand(cmEliminar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
+        public void EliminarVideo(int cod_curso, int cod_video)
+        {
+            objConectar.Abrir();
+            string cmEliminar = "comando";
+            SqlCommand comando = new SqlCommand(cmEliminar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
+        public void EditarUsuario(int cod,string correo, string password) 
+        {
+            objConectar.Abrir();
+            string cmEditar = "comando";
+            SqlCommand comando = new SqlCommand(cmEditar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
+        public void EditarCurso(int codigo, string titulo, string lenguaje, string descripcion, string precio)
+        {
+            objConectar.Abrir();
+            string cmEditar = "comando";
+            SqlCommand comando = new SqlCommand(cmEditar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
+        public void EditarVideo(int cod_curso,int cod_video, string titulo, string duracion)
+        {
+            objConectar.Abrir();
+            string cmEditar = "comando";
+            SqlCommand comando = new SqlCommand(cmEditar, objConectar.conectar);
+            comando.ExecuteNonQuery();
+            objConectar.Cerrar();
+        }
+
     }
 }
--- APLICACIONBASEDEDATOS/Capa_Logica/OperacionesLL.cs
+++ APLICACIONBASEDEDATOS/Capa_Logica/OperacionesLL.cs
@@ -3,10 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Capa_BD;
 
 namespace Capa_Logica
 {
     class OperacionesLL
     {
+        private OperacionesBD objOperaciones = new OperacionesBD();
+
+        public DataTable MostrarUsuarios()
+        {
+            DataTable tabla = objOperaciones.MostrarUsuarios();
+            return tabla;
+            
+        }
+
+        public DataTable MostrarCursos()
+        {
+            DataTable tabla = objOperaciones.MostrarCursos();
+            return tabla;
+        }
+
+        public DataTable MostrarVideos(int cod_curso)
+        {
+            DataTable tabla = objOperaciones.MostrarVideos(cod_curso);
+            return tabla;
+        }
+
+        public DataTable MostrarComprados(int cod_curso)
+        {
+            DataTable tabla = objOperaciones.MostrarVideos(cod_curso);
+            return tabla;
+        }
+
+        public DataTable BuscarUsuario(string nomb)
+        {
+            DataTable tabla = objOperaciones.BuscarUsuario(nomb);
+            return tabla;
+
+        }
+
+        public DataTable BuscarCurso(int cod)
+        {
+            DataTable tabla = objOperaciones.MostrarVideos(cod);
+            return tabla;
+        }
+
+        public void IngresarUsuario(string nombre, string correo, string password)
+        {
+            objOperaciones.IngresarUsuario(nombre, correo, password);
+        }
+
+        public void IngresarCurso(string titulo, string lenguaje, string descripcion, string precio)
+        {
+            objOperaciones.IngresarCurso(titulo, lenguaje, descripcion, precio);
+        }
+
+        public void IngresarVideo(string titulo, string duracion)
+        {
+            objOperaciones.IngresarVideo(titulo, duracion);
+        }
+        public void CursoComprado(int cod_usu, int cod_video)
+        {
+            objOperaciones.CursoComprado(cod_usu, cod_video);
+        }
+
+        public void EliminarUsuario(int cod)
+        {
+            objOperaciones.EliminarUsuario(cod);
+        }
+
+        public void EliminarCurso(int cod)
+        {
+            objOperaciones.EliminarCurso(cod);
+        }
+
+        public void EliminarVideo(int cod_curso, int cod_video)
+        {
+            objOperaciones.EliminarVideo(cod_curso, cod_video);
+        }
+
+        public void EditarUsuario(int cod,string correo, string password)
+        {
+            objOperaciones.EditarUsuario(cod,correo, password);
+        }
+
+        public void EditarCurso(int codigo, string titulo, string lenguaje, string descripcion, string precio)
+        {
+            objOperaciones.EditarCurso(codigo, titulo, lenguaje, descripcion, precio);
+        }
+
+        public void EditarVideo(int cod_curso, int cod_video, string titulo, string duracion)
+        {
+            objOperaciones.EditarVideo(cod_curso, cod_video, titulo, duracion);
+        }
     }
 }
